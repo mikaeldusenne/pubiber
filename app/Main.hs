2153fba3acba1a5b3f2f36a17752378b74747c47
Mikaunix <mikaunix@gmail.com>
2018-02-14 22:10:37 +0100

    added possibility to have a manual file; use of optparse-args; notes.org

--- app/Main.hs
+++ app/Main.hs
@@ -1,10 +1,71 @@
+{-# LANGUAGE ApplicativeDo              #-}
 module Main where
 
 import System.Environment
 import Lib
 import List
+import General (orelse)
+
+import System.FilePath.Posix
+import System.Directory
+import Options.Applicative
+import qualified Options.Applicative as O (header)
+
+import Data.Semigroup ((<>))
+
+
+data SettingsOpts = SettingsOpts {
+  dir :: String,
+  from :: String,
+  to :: String,
+  manual :: String
+  }
+  deriving (Show, Read)
+
+
+opts :: Parser SettingsOpts
+opts = SettingsOpts
+  <$> argument str (metavar "DIR")
+  <*> f "from" 'i' "txt file with PMIDs"           "references.txt"
+  <*> f "to"   'o' ".bib file to generate"         "references.bib"
+  <*> f "also" '+' ".bib file to append to output" "manual.bib"
+  where f l s h v = strOption $ long l <> short s <> help h <> value v
+
+
+
+
+parseFile :: String -> IO [[String]]
+parseFile path =
+  let f = map (\(ML k v) -> v)
+  in (transpose.map f.readArticles . stripcomments <$> readFile path)
 
 main :: IO ()
-main = getArgs
-  >>= (\(from:to:_) ->
-         concatWith "\n\n" <$>file_to_bib from >>=  writeFile to )
+main = do
+  let soptsP = info (opts <**> helper)
+        (fullDesc <> progDesc "generate bibtex from PMIDs")
+  (SettingsOpts{dir=dir, from=from, to=to, manual=manual}) <- execParser soptsP
+
+
+  [ids,nicks,comment] <- parseFile from
+
+  putStrLn $ show (length ids) ++ " articles to fetch."
+  
+  let createNote name = writeFile (dir </> "notes" </> name <.> "org")
+  traverse (uncurry createNote) $ zip nicks comment
+
+  let bibz = ((dropWhile (=='\n')<$>) . pubmed_fetch_id $ concatWith "," ids)
+             >>= ( return . request'result'to'biber nicks comment)
+  
+  pubmed_bibs <- concatWith "\n\n" <$> bibz
+  manual_bibs <- doesFileExist manual >>= (readFile manual
+                                                  `orelse` mempty)
+  writeFile to $ pubmed_bibs <> "\n\n" <> manual_bibs
+  -- >>= (\(from:to:_) ->
+  --         >>=  writeFile to )
+  putStrLn "ok"
+
+
+-- main = getArgs
+--   >>= (\(from:to:_) ->
+--          concatWith "\n\n" <$> file_to_bib from >>=  writeFile to )
+
--- src/Lib.hs
+++ src/Lib.hs
@@ -1,38 +1,52 @@
 {-# LANGUAGE OverloadedStrings #-}
 module Lib where
 
-import General (just_or_default,fromJust,(!∫),alphaUpper)
+import General -- (just_or_default,fromJust,(!∫),(?),alphaUpper)
 import List
-import Tuple(applyToFst, uncurry3)
+import Tuple
 import Control.Monad(join)
-import Parser(many)
 import Text.Read(readMaybe)
 import Network.HTTP.Client
 import Data.ByteString.Lazy.Char8(unpack,pack)
 import Requester
-
+import System.FilePath.Posix
 ---- Utils ----
+
+months = words $ "Jan Feb Mar Apr May Jun Jul Aug Sep Oct Nov Dec"
+
+many :: ReadS α -> ReadS [α]
+many read = just_or_default []
+  . nothingIf (isEmpty.fst.head)
+  . f []
+  where f acc [] = [(acc,"")]
+        f acc r  = case read r of [] -> [(acc,r)]
+                                  [(e,s)] -> f (acc++[e]) s
+
+
+fromReads = (>>= (\(a,b) -> if isEmpty b
+                            then Just a
+                            else Nothing))
+            . safe_head 
+
 
 nomaybe = just_or_default "(x)"
 notempty = (const "(x)" !∫ id) . isEmpty
-
 
 ---- Data ----
 
 data Medline = ML {header::String, content::String}
 
 instance Read Medline where
-  readsPrec _ r = [ (ML h c,xs) | (h,s) <- lex r,
+  readsPrec _ r = [ (ML h c,xs) | (h,s) <- rh r,
                     ("-",s') <- lex s,
                     (c,xs) <- rb [] s' ]
+                  
     where rh = (:[]) . span (`elem`alphaUpper)
           rb acc [] = [(acc,"")]
           rb acc s  = if beginWith " " l
                       then (rb (acc++f acc l) ls)
                       else [(acc,s)]
-            where (l,ls) = f . span (/='\n') $ s
-                    where f (a,[]) = (a,[])
-                          f (a,(x:xs)) = (a,xs)
+            where (l,ls) = sliceOn '\n' $ s
                   f e = (if e==[] then id else (' ':))
                     . dropWhile (==' ')
 
@@ -41,19 +55,25 @@ instance Show Medline where
 
 ---- constants ----
 
+path = "/home/mika/Documents/random/sante_publique/cismef/article_pubmed_search_optimization/bibliographie"
+
 -- pubmed_fetch_id s = exec "efetch" .
 --   append s . words $ "-db pubmed -format medline -id"
 
 elements :: [(String, [Medline] -> Maybe String)]
 elements = uncurry zip
-  . applyToFst allToMaxSize . unzip . map f $ es
+  . applyToFst (allToMaxSize ' ') . unzip
+  . map f $ es
   where f (a,b,c) = (a, c b)
         -- type F = String -> [Medline] -> Maybe String
         -- es :: [(String,String,F)]
         es = [("author"   ,"FAU", ((concatWith " and " <$>) .) . findAll),
               ("title"    ,"TI",  find),
               ("publisher","JT",  find),
-              ("date"     ,"DP",  ((>>=parseDate).).find)]
+              ("journal"  ,"TA",  find),
+              ("date"     ,"DP",  ((>>=parseDate).).find),
+              ("month"    ,"DP",  ((>>=parseMonth).).find)
+             ]
 
 ---- Functions ----
 
@@ -61,6 +81,12 @@ elements = uncurry zip
 -- readMLs :: String -> [Medline]
 readMLs = fst . head . many (reads::ReadS Medline)
 
+readsArticle :: ReadS [Medline]
+readsArticle = map f . many (reads :: ReadS Medline)
+  where f = applyToSnd $ dropWhile (=='\n') 
+
+readArticles :: String -> [[Medline]]
+readArticles = fromJust . fromReads . many readsArticle
 
 -- findAll :: String -> [Medline] -> Maybe [String]
 findAll h = nothingIf isEmpty . (content <$>)
@@ -68,14 +94,19 @@ findAll h = nothingIf isEmpty . (content <$>)
 
 -- find :: String -> [Medline] -> Maybe String
 find h = join . (safe_head<$>) . findAll h
+find' h = fromJust . (head<$>) . findAll h
 
 
 -- parseDate :: String -> Maybe String
-parseDate = safe_head . filter isY . words
-  where isY s = and [i/=Nothing,
-                     (>1000)$fromJust i]
-          where i = (readMaybe::String-> Maybe Int) s
-          
+-- parseDate = safe_head . filter isY . words
+--   where isY s = and [i/=Nothing,
+--                      (>1000)$fromJust i]
+--           where i = (readMaybe::String-> Maybe Int) s
+
+-- https://www.nlm.nih.gov/bsd/mms/medlineelements.html#dp
+parseDate = safe_head . words
+
+parseMonth = (>>=safe_head) . safe_tail . words
 
 -- tobibnick :: String -> [Medline] -> String
 tobibnick nick comment l = unlines
@@ -83,7 +114,7 @@ tobibnick nick comment l = unlines
   . map  ("    "++)
   . add "keywords" "trusted"
   . add "comment" comment
-  . (fromJust<$>) . filter (/=Nothing) . map f $ elements
+  . (fromJust<$>) . filter isJust . map f $ elements
   where 
         first = "@article{" ++nick++","
         f (a,b) =  ((a ++) . curly<$>) . b $ l
@@ -99,16 +130,12 @@ pubmed_fetch_id = (>>=send).build POST fetchurl
   where fetchurl = "https://eutils.ncbi.nlm.nih.gov/entrez/eutils/efetch.fcgi"
     
 
--- :: [String] -> String -> [String]
+request'result'to'biber :: [String] ->  [String] -> String -> [String]
 request'result'to'biber nicks comment =
   map (uncurry3 tobibnick)
-  . zip3 nicks comment -- 
-  . map (readMLs.unlines) -- parse each record
-  .splitOn (==[]).lines -- separate each record (empty line)
+  . zip3 nicks comment 
+  . map readMLs
+  . splitWhen "\n\n" -- separate each record (empty line)
 
+stripcomments = unlines.filter (not.(==Just '#').safe_head) .lines
 
-file_to_bib :: String -> IO [String]
-file_to_bib path =
-  ((transpose.map (splitOn (=='|')).lines) <$> readFile path)
-  >>= (\[ids,nicks,comment] -> (pubmed_fetch_id $ concatWith "," ids)
-        >>= ( return . request'result'to'biber nicks comment))
